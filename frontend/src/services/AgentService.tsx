a7c12c723cc54e6340a83a072fa4a0ddbc8344a7
Henrik <henrrb@stud.ntnu.no>
2023-10-09 23:44:52 +0200

    Add function call to API in frontend

---
+++ frontend/src/services/AgentService.tsx
@@ -0,0 +1,12 @@
+import { BACKEND_URL } from "../utils";
+import axios from "axios";
+
+const getAPIUrl = (id?: number) => {
+    return `${BACKEND_URL}`;
+}
+
+export const makeQuery = async (query?: string) => {
+    const url = getAPIUrl();
+    const queryJSON = JSON.stringify(query);
+    return await axios.post(url, queryJSON);
+}

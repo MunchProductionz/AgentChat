4893d4fe1712c1b79cdfcf80b803ee8ee3b7057a
Henrik Bø <61332066+MunchProductionz@users.noreply.github.com>
2023-10-09 22:57:26 +0200

    3 UI create UI (#9)
    * Add frontend using standard react app in typescript
    
    * Add frontend with input field, button and output field
    
    * Update README

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chat from './components/chat/Chat';
+
+function App() {
+  return (
+    <Chat/>
+  );
+}
+
+export default App;

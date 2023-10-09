4618e6c1730ba8afeb0488b442353803b1043350
Henrik <henrrb@stud.ntnu.no>
2023-10-10 00:00:21 +0200

    Update access to .env file

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -1,6 +1,6 @@
 const BACKEND_URL_ENV_CONFIG = "AWS_API_URL";
 const getBackendUrl = () => {
-  return import.meta.env[BACKEND_URL_ENV_CONFIG];
+  return process.env[BACKEND_URL_ENV_CONFIG];
 };
 
 export const BACKEND_URL = getBackendUrl();

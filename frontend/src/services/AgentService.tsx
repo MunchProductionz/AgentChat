6cbd9cb92f245e3f04c28aeb2b8d42849b262573
Henrik <henrrb@stud.ntnu.no>
2023-10-11 15:46:00 +0200

    Read environment variables

--- frontend/src/services/AgentService.tsx
+++ frontend/src/services/AgentService.tsx
@@ -1,6 +1,5 @@
 import { authenticatedClient } from "../authentication/http_client";
 import { BACKEND_URL } from "../utils";
-import dotenv from '../dotenv-setup';
 
 const getAPIUrl = () => {
     return `${BACKEND_URL}/`;
@@ -8,12 +7,9 @@ const getAPIUrl = () => {
 
 export const makeQuery = async (query?: string) => {
     const url = getAPIUrl();
-    console.log(url)
     const userQuery = {
         query: query
     }
-    const userQueryJSON = JSON.stringify(userQuery);
-    console.log(`Querying http://127.0.0.1:8000/ with ${userQueryJSON}`);
-    
-    return await (await authenticatedClient).post(`http://127.0.0.1:8000/`, userQueryJSON);
+    const userQueryJSON = JSON.stringify(userQuery);    
+    return await (await authenticatedClient).post(url, userQueryJSON);
 }
--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -1,4 +1,7 @@
-const BACKEND_URL_ENV_CONFIG = "AWS_API_URL";
+// console.log(__dirname);
+// dotenv.config({path: path.resolve(__dirname, "../.env")})
+
+const BACKEND_URL_ENV_CONFIG = "REACT_APP_AWS_API_URL";
 const getBackendUrl = () => {
   return process.env[BACKEND_URL_ENV_CONFIG];
 };
